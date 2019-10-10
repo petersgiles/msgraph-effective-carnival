5bfd462e2d6cdcccfbb5b87a502523d0633e9e4c
Peter Giles <peter.s.giles@gmail.com>
2019-10-10 11:35:51 +1100

    initial

---
+++ src/MsGraphConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsGraphConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
